c780212cdeb9c8703863c6e975718b41b1f4268f
mainangaruiya <003paulmaina@gmail.com>
2023-12-05 12:42:14 +0300

    fix

---
+++ website/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+	return render_template("home.html")
+
+@views.route('/login')
+def login():
+	return render_template("login.html")
+
+@views.route('/logout')
+def logout():
+	return "<P>Logout</p>"
+
+@views.route('/sign_up')
+def sign_up():
+	return render_template("sign_up.html")
