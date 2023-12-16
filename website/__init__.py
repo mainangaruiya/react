d738757ab9ca33c17b56708f8510e7a098e087d5
mainangaruiya <003paulmaina@gmail.com>
2023-12-17 00:35:23 +0300

    fix features

--- website/__init__.py
+++ website/__init__.py
@@ -3,34 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-app = Flask(__name__)
-
-# Configure app, login manager, and database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['STATIC_FOLDER'] = 'static'  # Correct indentation
-
+    app.config['STATIC_FOLDER'] = 'static'
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///:memory:'
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
-    from .models import User, Note
-    with app.app_context():
-        db.create_all()
+    app.register_blueprint(views, url_prefix='')
+    app.register_blueprint(auth, url_prefix='')
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -43,6 +30,7 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+    with app.app_context():
+        if not path.exists('website/' + DB_NAME):
+            db.create_all()
+            print('Created Database!')
