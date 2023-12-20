abc846dd91135bd7d9d8660e97ba4113da85cff1
mainangaruiya <003paulmaina@gmail.com>
2023-12-20 18:45:10 +0300

    commit

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from flask import render_template, request ,flash 
-from website import create_app
-from website.models import db, Property, User  # Import db from models.py
+from flask import render_template, request, flash, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from website.views import views
 from werkzeug.utils import secure_filename
+from website import create_app
+from website.models import db, Property, User
+import os
 
 app = create_app()
-app.register_blueprint(views, name='views_blueprint')
-
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
@@ -15,23 +13,31 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Your existing routes and configurations...
+def save_and_get_filename(file):
+    if file:
+        filename = secure_filename(file.filename)
+        upload_folder = os.path.join(app.root_path, 'properties')
+        os.makedirs(upload_folder, exist_ok=True)
+        file.save(os.path.join(upload_folder, filename))
+        return filename
+    return None
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
 @app.route('/login', methods=['POST'])
 def login():
-    user = get_user_somehow()
+    user = get_user_somehow()  # Implement this function to get the user
     login_user(user)
+    flash('Login successful!', 'success')
     return redirect(url_for('views.buy'))
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
+    flash('Logout successful!', 'success')
     return render_template('logout.html')
 
 @app.route('/sign-up')
@@ -41,68 +47,41 @@ def sign_up():
 @app.route('/account')
 @login_required
 def account():
-    # Retrieve user information
     user_email = current_user.email
-
-    # Retrieve properties posted by the current user
     user_properties = Property.query.filter_by(user_id=current_user.id).all()
+    return render_template('account.html', user_email=user_email, user_properties=user_properties)
 
-    return render_template('account.html',user_email=user_email, user_properties=user_properties)
-
-
-@app.route('/sell')
+@app.route('/sell', methods=['GET', 'POST'])
 @login_required
 def sell():
     if request.method == 'POST':
         try:
-            # Retrieve form data
-            kitchen_image = request.form['kitchenImage']
-            sitting_room_image = request.form['sittingRoomImage']
-            master_bedroom_image = request.form['masterBedroomImage']
-            
-            # Retrieve additional images dynamically
-            additional_images = []
-            for i in range(1, 4):
-                image_key = f'additionalImage{i}'
-                if image_key in request.form:
-                    additional_images.append(request.form[image_key])
-
-            # Retrieve other form data
             property_title = request.form['propertyTitle']
             num_bedrooms = request.form['numBedrooms']
             property_location = request.form['propertyLocation']
-            price = request.form['price']  # Add this line to retrieve the price
+            price = request.form['price']
 
-            # Perform any additional logic or validation here
+            property_video_file = request.files['propertyVideo']
+            property_video = save_and_get_filename(property_video_file)
 
-            # Create a new Property instance
             new_property = Property(
-                kitchen_image=kitchen_image,
-                sitting_room_image=sitting_room_image,
-                master_bedroom_image=master_bedroom_image,
-                additional_images=','.join(additional_images),
                 property_title=property_title,
                 num_bedrooms=num_bedrooms,
                 property_location=property_location,
                 price=price,
+                property_video=property_video,
                 user_id=current_user.id
             )
 
-            # Add the new property to the database
             db.session.add(new_property)
             db.session.commit()
 
-            # Flash success message
             flash('Property added successfully!', 'success')
-
-            # Redirect to another page or render template
             return redirect(url_for('views.sell'))
 
         except Exception as e:
-            # Flash error message
             flash(f'Error adding property: {str(e)}', 'error')
 
-    # If the request method is GET, simply render the sell.html template
     return render_template('sell.html')
 
 def save_and_get_filename(file):
@@ -112,16 +91,8 @@ def save_and_get_filename(file):
         return filename
     return None
 
-@app.route('/buy', methods=['GET'])
-@login_required
-def buy():
-    # Retrieve properties from the database
-    all_properties = Property.query.all()
-
-    # Render the buy.html template and pass the properties to it
-    return render_template('buy.html', all_properties=all_properties)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Create the database tables only if they don't exist
+        db.create_all()
     app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note, Property
+from .models import Property
 from . import db
-import json
 
 views = Blueprint('views', __name__)
 
@@ -10,79 +9,43 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
-
 @views.route('/sell', methods=['GET', 'POST'])
 @login_required
 def sell():
     if request.method == 'POST':
         try:
-            # Retrieve form data
-            kitchen_image = request.form['kitchenImage']
-            sitting_room_image = request.form['sittingRoomImage']
-            master_bedroom_image = request.form['masterBedroomImage']
-            
-            # Retrieve additional images dynamically
-            additional_images = []
-            for i in range(1, 4):
-                image_key = f'additionalImage{i}'
-                if image_key in request.form:
-                    additional_images.append(request.form[image_key])
-
-            # Retrieve other form data
             property_title = request.form['propertyTitle']
             num_bedrooms = request.form['numBedrooms']
             property_location = request.form['propertyLocation']
-            price = request.form['price']  # Add this line to retrieve the price
+            price = request.form['price']
 
-            # Perform any additional logic or validation here
+            property_video_file = request.files['propertyVideo']
+            property_video = save_and_get_filename(property_video_file)
 
-            # Create a new Property instance
             new_property = Property(
-                kitchen_image=kitchen_image,
-                sitting_room_image=sitting_room_image,
-                master_bedroom_image=master_bedroom_image,
-                additional_images=','.join(additional_images),
                 property_title=property_title,
                 num_bedrooms=num_bedrooms,
                 property_location=property_location,
                 price=price,
+                property_video=property_video,
                 user_id=current_user.id
             )
 
-            # Add the new property to the database
             db.session.add(new_property)
             db.session.commit()
 
-            # Flash success message
             flash('Property added successfully!', 'success')
-
-            # Redirect to another page or render template
             return redirect(url_for('views.sell'))
 
         except Exception as e:
-            # Flash error message
             flash(f'Error adding property: {str(e)}', 'error')
 
-    # If the request method is GET, simply render the sell.html template
     return render_template('sell.html')
 
 @views.route('/buy', methods=['GET'])
 @login_required
 def buy():
     all_properties = Property.query.all()
-
-    # Render the buy.html template and pass the properties to it
     return render_template('buy.html', all_properties=all_properties)
 
 @views.route('/login', methods=['POST'])
@@ -90,24 +53,20 @@ def login():
     login_user(user)
     return redirect(url_for('views.buy'))
 
-
 @views.route('/logout')
 @login_required
 def logout():
     logout_user()
+    flash('Logout successful!', 'success')
     return render_template('logout.html')
 
 @views.route('/sign-up')
 def sign_up():
     return render_template('sign-up.html')
 
-
 @views.route('/account')
 @login_required
 def account():
-    # Use the `current_user` provided by Flask-Login
     user_email = current_user.email if current_user.is_authenticated else None
-    # Retrieve properties posted by the current user
     user_properties = Property.query.filter_by(user_id=current_user.id).all()
-
     return render_template('account.html', user_email=user_email, user_properties=user_properties)
